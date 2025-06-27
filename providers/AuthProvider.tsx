5060e7ac4de4156adc4df3a8585669cca0ba7abd
kripesh <kripesholi3@gmail.com>
2025-06-27 14:56:55 +0545

    Fix import paths for types

--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { createContext, useContext, useEffect, useState } from 'react';
-import { User } from '../types';
+import { User } from '../app/types';
 import api from '../lib/api';
 import { toast } from 'react-hot-toast';
 
